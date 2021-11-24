8e25173386c731220ddc91ffb97e9cd4998c3e7d
Evan Burton <butonevan1@gmaail.com>
2021-11-24 10:17:39 -0700

    got blog to post as well as image

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-main>
+    <v-container>
+      <h2>{{ post.title }}</h2>
+       <nuxt-img :src="post.image" sizes="sm:100vw md:50vw lg:600px" />
+      <nuxt-content :document="post" />
+    </v-container>
+  </v-main>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const post = await $content(`blog/${params.slug}`).fetch();
+      return {
+        post,
+      };
+    } catch (error) {
+      error("No article found");
+    }
+  },
+};
+</script>

bee8abeba05b4b7e91f83cda4891c215084e8959
Evan Burton <butonevan1@gmaail.com>
2021-11-24 12:29:06 -0700

    timezone

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   
   head: {
-    titleTemplate: '%s - 200-a1',
-    title: '200-a1',
+    titleTemplate: '%s - 200-a3',
+    title: '200-a3',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
